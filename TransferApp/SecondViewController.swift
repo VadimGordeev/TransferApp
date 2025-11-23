98da5e3196ff3191bfc1f71d39e35455e422c94d
VadimGordeev <vadimgordeev22@gmail.com>
2025-11-23 15:01:44 +0100

    feat: create app with data transfer with options

---
+++ TransferApp/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  TransferApp
+//
+//  Created by Vadim on 23/11/2025.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet var dataTextField: UITextField!
+    var updatingData: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateTextFieldData(withText: updatingData)
+    }
+    
+    private func updateTextFieldData(withText text: String) {
+        dataTextField.text = text
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TransferApp/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TransferApp
+//
+//  Created by Vadim on 23/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var dataLabel: UILabel!
+
+    @IBAction func editDataWithProperty(_ sender: UIButton) {
+//        получаем вью контроллер, в который происходит переход
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let editScreen = storyboard.instantiateViewController(
+            withIdentifier: "SecondViewController"
+        ) as! SecondViewController
+        
+//        передаем данные
+        editScreen.updatingData = dataLabel.text ?? ""
+        
+//        переходим к следующему экрану
+        self.navigationController?.pushViewController(editScreen, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
